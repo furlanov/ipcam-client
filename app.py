ed5a7497b8ac45186c99a0d466ba6018fe10ddfb
Nik Furlan <nik@furlan>
2024-08-28 11:05:15 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+url = ""
+username = ""
+password = ""
+
+window = tk.Tk()
+window.title("SCam")
+window.geometry("320x170")
+window.resizable(True, True)
+
+image_label = tk.Label(window)
+image_label.pack(fill=tk.BOTH, expand=True)
+
+def update_camera_feed():
+    try:
+        response = requests.get(url, auth=(username, password), stream=True)
+        response.raise_for_status()
+        
+        image = Image.open(BytesIO(response.content))
+        
+        window_width = window.winfo_width()
+        window_height = window.winfo_height()
+        resized_image = image.resize((window_width, window_height))
+        
+        tk_image = ImageTk.PhotoImage(resized_image)
+        
+        image_label.config(image=tk_image)
+        image_label.image = tk_image
+    except Exception as e:
+        print("Error:", e)
+    
+    window.after(500, update_camera_feed)
+
+update_camera_feed()
+
+window.mainloop()
